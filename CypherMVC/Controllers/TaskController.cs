70ad03e58a0087f9327582ee2a7ac94e65880fcb
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-14 22:33:32 -0400

    Create task controller

---
+++ CypherMVC/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CypherMVC.Controllers
+{
+    public class TaskController : Controller
+    {
+      
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEdit()
+        {
+            return View();
+        }
+    }
+}
