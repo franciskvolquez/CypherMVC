0fa680bd3d02090578e52da0b2af3177f0a21f9f
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-14 22:32:16 -0400

    Create messages controller

---
+++ CypherMVC/Controllers/MessagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CypherMVC.Controllers
+{
+    public class MessagesController : Controller
+    {
+        
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+
+        public ActionResult Reply()
+        {
+            return View();
+        }
+    }
+}
