abdc243cc2db93e2c93c51502c0c41c07bb824e1
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-14 23:27:11 -0400

    Include bundles in views

--- CypherMVC/App_Start/BundleConfig.cs
+++ CypherMVC/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace CypherMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").IncludeDirectory("~/Content", "*.css"));
         }
     }
 }
