019d05416635123d45e2d37c2af0dafffff44dbe
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-15 21:11:54 -0400

    Add TaskController createEdit funcionality

--- CypherMVC/Controllers/TaskController.cs
+++ CypherMVC/Controllers/TaskController.cs
@@ -15,18 +15,55 @@ namespace CypherMVC.Controllers
             return View();
         }
 
-        public ActionResult CreateEdit()
+        public ActionResult CreateEdit(int id = 0)
         {
-            var list = new List<SelectListItem>()
-            {
-               new SelectListItem() {Text = "Business Enquiry", Value = "4" },
-               new SelectListItem() {Text = "Payment", Value= "1"},
-               new SelectListItem() {Text = "UI Issue", Value= "2"},
-               new SelectListItem() {Text = "Question", Value= "3"},
-            };
+            var context = new FeedbackContext();
 
-            ViewBag.Categories = list;
+            ViewBag.Categories = context.Categories.Select(
+                x => new SelectListItem() { Text = x.Name, Value = x.Id.ToString() }).ToList();
+            
+            if (id != 0)
+            {
+                return View(context.Tasks.FirstOrDefault(x => x.Id == id));
+            }
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEdit(Task task)
+        {
+            var context = new FeedbackContext();
+
+            if(ModelState.IsValid)
+            {
+                var editTask = context.Tasks.FirstOrDefault(x => x.Id == task.Id);
+                if (editTask != null)
+                {
+                    //Updating task 
+                    editTask.Title = task.Title;
+                    editTask.Description = task.Description;
+                    editTask.AssignedToId = task.AssignedToId;
+                    editTask.DueDate = task.DueDate;
+                    editTask.AssociatedMessageId = task.AssociatedMessageId;
+                    editTask.Completed = task.Completed;
+                    editTask.Notes = task.Notes;
+                    context.Entry(editTask).State = System.Data.Entity.EntityState.Modified;
+                }
+                else
+                {
+                    //New Task
+                    context.Tasks.Add(task);
+                    task.Created = DateTime.Now;
+                }
+                context.SaveChanges();
+                return RedirectToAction("ViewAll");
+            }
+
+            ViewBag.Categories = context.Categories.Select(
+                x => new SelectListItem() { Text = x.Name, Value = x.Id.ToString() }).ToList();
+
+            return View(task);
         }
     }
 }
