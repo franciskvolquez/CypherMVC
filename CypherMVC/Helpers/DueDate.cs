6626d002ba9adb08d990caa1c87e93446c0ec5c8
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-16 21:44:02 -0400

    Add validation to Task model

---
+++ CypherMVC/Helpers/DueDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CypherMVC.Helpers
+{
+    public class DueDate: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            try
+            {
+                return (DateTime)value >= DateTime.Now;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        
+        
+    }
+}
--- CypherMVC/Models/Task.cs
+++ CypherMVC/Models/Task.cs
@@ -1,20 +1,32 @@
-﻿using System;
+﻿using CypherMVC.Helpers;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace CypherMVC.Models
 {
     [Bind(Exclude ="AssignedTo, AssociatedMessage, Category")]
-    public class Task
+    public class Task : IValidatableObject
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
+        [DueDate(ErrorMessage = "Date must be in the future")]
         public DateTime? DueDate { get; set; }
+        [Required]
         public int AssignedToId { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public int AssociatedMessageId { get; set; }
         public DateTime? Created { get; set; }
+        [StringLength(1000,MinimumLength =20)]
         public string Notes { get; set; }
+        [Required]
         public bool Completed { get; set; }
 
 
@@ -23,6 +35,16 @@ namespace CypherMVC.Models
         public virtual Message AssociatedMessage { get; set; }
         public virtual Category Category { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
 
+            if(Completed && string.IsNullOrWhiteSpace(Notes))
+            {
+                errors.Add(new ValidationResult("Notes are required when completing a task"));
+            }
+
+            return errors;
+        }
     }
 }
