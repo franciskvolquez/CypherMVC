a7c0b18e52bfc101abb344f8870227d95db10435
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-16 22:37:41 -0400

    Add survey and suggestions funcionality

--- CypherMVC/Controllers/HomeController.cs
+++ CypherMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CypherMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,54 @@ namespace CypherMVC.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Survey()
+        {
+            var context = new FeedbackContext();
+            var admins = context.Admins.OrderByDescending(x => x.Votes.Count).ToList();
+
+            if(Session["HasVoted"] != null)
+            {
+                return PartialView("SurveyResults", admins);
+            }
+
+            return PartialView(admins);
+        }
+
+        [HttpPost]
+        public ActionResult Survey(int adminId)
+        {
+            var context = new FeedbackContext();
+            context.Votes.Add(new Vote() { AdminId = adminId });
+            context.SaveChanges();
+
+            Session["HasVoted"] = true;
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Suggestion()
+        {
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public ActionResult Suggestion(string suggestion)
+        {
+            if(string.IsNullOrWhiteSpace(suggestion))
+            {
+                //Send email
+                TempData["Status"] = "Your message has been submitted!";
+            }
+            else
+            {
+                TempData["Status"] = "Your message could not be submitted!";
+            }
+
+
+            return RedirectToAction("Index");
         }
     }
 }
